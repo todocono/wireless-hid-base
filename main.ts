d48ccabfa92a248a47f193e4df894c191799a9e3
Rodolfo <todocono@gmail.com>
2022-12-21 12:00:39 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,23 +1,21 @@
-modules.gamepad1.onEvent(jacdac.GamepadEvent.ButtonsChanged, function () {
-    if (modules.gamepad1.isButtonDown(jacdac.GamepadButtons.A)) {
-        if (mouse_click) {
-            mouse_click = 0
-            modules.hidMouse1.setButton(jacdac.HidMouseButton.Left, jacdac.HidMouseButtonEvent.Up)
-            basic.pause(100)
-        } else {
-            mouse_click = 1
-            modules.hidMouse1.setButton(jacdac.HidMouseButton.Left, jacdac.HidMouseButtonEvent.Down)
-            basic.pause(100)
-        }
+radio.onReceivedValue(function (name, value) {
+    if (name == "x") {
+        modules.hidMouse1.move(value, 0)
+        led.plotBarGraph(
+        value,
+        0
+        )
+    } else if (name == "y") {
+        modules.hidMouse1.move(0, value)
+    } else if (name == "adown") {
+        modules.hidMouse1.setButton(jacdac.HidMouseButton.Left, jacdac.HidMouseButtonEvent.Down)
+    } else if (name == "aup") {
+        modules.hidMouse1.setButton(jacdac.HidMouseButton.Left, jacdac.HidMouseButtonEvent.Up)
+    } else {
+    	
     }
 })
-let mouse_click = 0
 radio.setGroup(1)
 basic.forever(function () {
-    if (Math.abs(modules.gamepad1.x()) >= 10) {
-        modules.hidMouse1.move(modules.gamepad1.x() / 2, 0)
-    }
-    if (Math.abs(modules.gamepad1.y()) >= 10) {
-        modules.hidMouse1.move(0, modules.gamepad1.y() / 2)
-    }
+	
 })
