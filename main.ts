7ec33cad8748ffaf919bd7f067b94b14ab9fb68c
Rodolfo <todocono@gmail.com>
2022-12-21 12:00:39 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,11 +2,11 @@ modules.gamepad1.onEvent(jacdac.GamepadEvent.ButtonsChanged, function () {
     if (modules.gamepad1.isButtonDown(jacdac.GamepadButtons.A)) {
         if (mouse_click) {
             mouse_click = 0
-            modules.hidMouse1.setButton(jacdac.HidMouseButton.Left, jacdac.HidMouseButtonEvent.Up)
+            radio.sendValue("adown", 1)
             basic.pause(100)
         } else {
             mouse_click = 1
-            modules.hidMouse1.setButton(jacdac.HidMouseButton.Left, jacdac.HidMouseButtonEvent.Down)
+            radio.sendValue("aup", 1)
             basic.pause(100)
         }
     }
@@ -15,9 +15,13 @@ let mouse_click = 0
 radio.setGroup(1)
 basic.forever(function () {
     if (Math.abs(modules.gamepad1.x()) >= 10) {
-        modules.hidMouse1.move(modules.gamepad1.x() / 2, 0)
+        radio.sendValue("x", modules.gamepad1.x() / 2)
+        led.plotBarGraph(
+        modules.gamepad1.x(),
+        0
+        )
     }
     if (Math.abs(modules.gamepad1.y()) >= 10) {
-        modules.hidMouse1.move(0, modules.gamepad1.y() / 2)
+        radio.sendValue("y", modules.gamepad1.y() / 2)
     }
 })
